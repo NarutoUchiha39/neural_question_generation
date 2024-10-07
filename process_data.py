6bfcfafc45fbd04be514a801eeb3331f630daf56
NarutoUchiha39 <104666748+NarutoUchiha39@users.noreply.github.com>
2024-10-07 19:52:58 +0530

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -20,7 +20,6 @@ question_outfile_dev = 'data/processed/dev_question.npy'
 sentence_outfile_test = 'data/processed/test_sentence.npy'
 question_outfile_test = 'data/processed/test_question.npy'
 
-
 maxlen_s_train = 60
 maxlen_q_train = 30
 
@@ -42,12 +41,11 @@ with open(TRAIN_SRC) as f:
 with open(TRAIN_TGT) as f:
     question_train = [line.split() for line in f.readlines()]
 
-maxlen = max([len(sentence) for sentence in sentence_train])
-print 'train sentences max length : %d '%maxlen
+maxlen = max(len(sentence) for sentence in sentence_train)
+print(f'train sentences max length : {maxlen}')
 
-maxlen = max([len(sentence) for sentence in question_train])
-print 'train questions max length : %d \n'%maxlen
-
+maxlen = max(len(sentence) for sentence in question_train)
+print(f'train questions max length : {maxlen}\n')
 
 # Dev data
 with open(DEV_SRC) as f:
@@ -56,11 +54,11 @@ with open(DEV_SRC) as f:
 with open(DEV_TGT) as f:
     question_dev = [line.split() for line in f.readlines()]
 
-maxlen = max([len(sentence) for sentence in sentence_dev])
-print 'dev sentences max length : %d '%maxlen
+maxlen = max(len(sentence) for sentence in sentence_dev)
+print(f'dev sentences max length : {maxlen}')
 
-maxlen = max([len(sentence) for sentence in question_dev])
-print 'dev questions max length : %d \n'%maxlen
+maxlen = max(len(sentence) for sentence in question_dev)
+print(f'dev questions max length : {maxlen}\n')
 
 # Test data
 with open(TEST_SRC) as f:
@@ -69,37 +67,36 @@ with open(TEST_SRC) as f:
 with open(TEST_TGT) as f:
     question_test = [line.split() for line in f.readlines()] 
     
-maxlen = max([len(sentence) for sentence in sentence_test])
-print 'test sentences max length : %d '%maxlen
+maxlen = max(len(sentence) for sentence in sentence_test)
+print(f'test sentences max length : {maxlen}')
 
-maxlen = max([len(sentence) for sentence in question_test])
-print 'test questions max length : %d \n'%maxlen
+maxlen = max(len(sentence) for sentence in question_test)
+print(f'test questions max length : {maxlen}\n')
 
 # <<< Read data and Check max-length
 
 # >>> Filtering data with max-length
 
-print 'restrict max length of train_sentence as %d'%maxlen_s_train
-print 'restrict max length of train_question as %d'%maxlen_q_train
-print 'restrict max length of dev_sentence as %d'%maxlen_s_dev
-print 'restrict max length of dev_question as %d'%maxlen_q_dev
-print 'restrict max length of test_sentence as %d'%maxlen_s_test
-print 'restrict max length of test_question as %d\n'%maxlen_q_test
+print(f'restrict max length of train_sentence as {maxlen_s_train}')
+print(f'restrict max length of train_question as {maxlen_q_train}')
+print(f'restrict max length of dev_sentence as {maxlen_s_dev}')
+print(f'restrict max length of dev_question as {maxlen_q_dev}')
+print(f'restrict max length of test_sentence as {maxlen_s_test}')
+print(f'restrict max length of test_question as {maxlen_q_test}\n')
 
 def filter_with_maxlen(maxlen_s, maxlen_q, sentence, question):
     # Filtering with maxlen(sentence)
-    temp_sentence = list()
-    temp_question = list()
+    temp_sentence = []
+    temp_question = []
     
     for i, line in enumerate(sentence):
-        if (len(line) <= maxlen_s):
+        if len(line) <= maxlen_s:
             temp_sentence.append(line)
             temp_question.append(question[i])
             
-
     # Filtering with maxlen(question)
-    filtered_sentence = list()
-    filtered_question = list()
+    filtered_sentence = []
+    filtered_question = []
 
     for i, line in enumerate(temp_question):
         if len(line) <= maxlen_q:
@@ -148,19 +145,19 @@ with open('data/processed/question_test.txt', 'w') as f:
 
 # Make vocab with filtered sentences and questions(train) >>>
 
-all_sentence  = filtered_sentence_train + filtered_question_train
+all_sentence = filtered_sentence_train + filtered_question_train
 
 # Make vocab with word frequency
 wordcount = defaultdict(int)
 for sentence in all_sentence:
     for word in sentence:
-        wordcount[word]+=1
-sorted_wordlist = [(k, wordcount[k]) for k in sorted(wordcount, key=wordcount.get, reverse=True)]
+        wordcount[word] += 1
+sorted_wordlist = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
 
-print 'resize dictionary with %d most frequent words...'%dic_size
+print(f'resize dictionary with {dic_size} most frequent words...')
 resized_dic = dict(sorted_wordlist[:(dic_size-4)])
 
-word2idx = dict()
+word2idx = {}
 word2idx['<PAD>'] = 0
 word2idx['<GO>'] = 1
 word2idx['<EOS>'] = 2
@@ -171,46 +168,44 @@ for word in resized_dic:
     idx += 1
 
 # Save dic
-print 'save Dic File...'
-with open(vocab_dir, 'w') as f:
+print('save Dic File...')
+with open(vocab_dir, 'wb') as f:
     pkl.dump(word2idx, f)
 
 # <<< Make vocab with filtered sentences and questions(train)
 
 # Process data with vocab >>>
-def process(data, vocab, maxlen, if_go = False):
+def process(data, vocab, maxlen, if_go=False):
     if if_go:
-        maxlen = maxlen + 2 # include <GO> and <EOS>
-    processed_data = list()
-    length_data = list()
+        maxlen = maxlen + 2  # include <GO> and <EOS>
+    processed_data = []
+    length_data = []
     for line in data:
-        processed_data.append([])
+        processed_line = []
         if if_go:
-            processed_data[-1].append(word2idx['<GO>'])
+            processed_line.append(word2idx['<GO>'])
         for word in line:
-            if word in word2idx:
-                processed_data[-1].append(word2idx[word])
-            else:
-                processed_data[-1].append(word2idx['<UNK>'])
+            processed_line.append(word2idx.get(word, word2idx['<UNK>']))
         if if_go:
-            processed_data[-1].append(word2idx['<EOS>'])
-        length_data.append(len(processed_data[-1])) 
-        processed_data[-1] = processed_data[-1] + [word2idx['<PAD>']] * (maxlen - len(processed_data[-1]))
+            processed_line.append(word2idx['<EOS>'])
+        length_data.append(len(processed_line))
+        processed_line += [word2idx['<PAD>']] * (maxlen - len(processed_line))
+        processed_data.append(processed_line)
     return processed_data, length_data
 
 # Train data
-processed_sentence_train, length_sentence_train = process(filtered_sentence_train, word2idx, maxlen_s_train, if_go = False)
-processed_question_train, length_question_train = process(filtered_question_train, word2idx, maxlen_q_train, if_go = True)
+processed_sentence_train, length_sentence_train = process(filtered_sentence_train, word2idx, maxlen_s_train, if_go=False)
+processed_question_train, length_question_train = process(filtered_question_train, word2idx, maxlen_q_train, if_go=True)
 
 # Eval data
-processed_sentence_dev, length_sentence_dev = process(filtered_sentence_dev, word2idx, maxlen_s_dev, if_go = False)
-processed_question_dev, length_question_dev = process(filtered_question_dev, word2idx, maxlen_q_dev, if_go = True)
+processed_sentence_dev, length_sentence_dev = process(filtered_sentence_dev, word2idx, maxlen_s_dev, if_go=False)
+processed_question_dev, length_question_dev = process(filtered_question_dev, word2idx, maxlen_q_dev, if_go=True)
 
 # Test data
-processed_sentence_test, length_sentence_test = process(filtered_sentence_test, word2idx, maxlen_s_test, if_go = False)
-processed_question_test, length_question_test = process(filtered_question_test, word2idx, maxlen_q_test, if_go = True)
+processed_sentence_test, length_sentence_test = process(filtered_sentence_test, word2idx, maxlen_s_test, if_go=False)
+processed_question_test, length_question_test = process(filtered_question_test, word2idx, maxlen_q_test, if_go=True)
 
-print 'Processing Complete'
+print('Processing Complete')
 # <<< Process data with vocab
 
 np.save(sentence_outfile_train, processed_sentence_train)
@@ -221,6 +216,4 @@ np.save(question_outfile_dev, processed_question_dev)
 
 np.save(sentence_outfile_test, processed_sentence_test)
 np.save(question_outfile_test, processed_question_test)
-print 'Saving Complete'
-
-
+print('Saving Complete')
